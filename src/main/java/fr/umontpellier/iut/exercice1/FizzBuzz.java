45c86292c39b065eae9b0cfce208f291748a36e0
Adrien Autef <adrienautef@etu.umontpellier.fr>
2019-02-05 17:10:18 +0100

    Exercice 1 Fizz Buzz complété

--- src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -3,10 +3,33 @@ package fr.umontpellier.iut.exercice1;
 public class FizzBuzz {
 
     public String getValue(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        String result;
+        if(i %3== 0 && i %5== 0){
+            System.out.println("FizzBuzz");
+            result="FizzBuzz";
+        }
+        else if(i %3 == 0) {
+            System.out.println("Fizz);");
+            result ="Fizz";
+        }
+        else if(i %5 == 0){
+            System.out.println("Buzz");
+            result="Buzz";
+        }
+        else{
+            System.out.println(i);
+            result=String.valueOf(i);
+        }
+        return result;
     }
 
     public String[] computeList(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        String[] result = new String[i];
+        int c;
+        for (c=0;c<i;c++){
+            result[c]= getValue(c+1);
+        }
+        return result;
     }
+
 }
